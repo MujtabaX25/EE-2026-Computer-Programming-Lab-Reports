5c9c8d6ae838c3b40b0b84737fa59364c5320ea9
Ahmad Mujtaba <crzmnt10@gmail.com>
2026-03-11 05:59:36 +0500

    Add files via upload

---
+++ EE-2026-Computer-Programming-Lab-Reports-main/Lab 5/tasks cpps/Using do-while Loop.cpp
@@ -0,0 +1,19 @@
+/* Name: Ahmad Mujtaba*/
+/* Roll No: 5125370 / BF25NWELE0706 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while(i <= 10);
+
+    return 0;
+}
---
+++ EE-2026-Computer-Programming-Lab-Reports-main/Lab 5/tasks cpps/While Loop.cpp
@@ -0,0 +1,18 @@
+/* Name: Ahmad Mujtaba*/
+/* Roll No: 5125370 / BF25NWELE0706 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
