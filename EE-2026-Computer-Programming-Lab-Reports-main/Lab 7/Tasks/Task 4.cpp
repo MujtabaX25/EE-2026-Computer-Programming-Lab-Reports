0da273e3f1537e7c60ff3e30de26fa47a0867b38
Ahmad Mujtaba <crzmnt10@gmail.com>
2026-06-03 07:26:11 +0500

    Add files via upload

---
+++ EE-2026-Computer-Programming-Lab-Reports-main/Lab 7/Tasks/Task 4.cpp
@@ -0,0 +1,20 @@
+/*Name: Ahmad Mujtaba
+  Roll No:BF25NWELE0706*/
+
+#include <iostream> 			// Include library
+using namespace std; 			// Use namespace
+int main() { 				// Main function
+int arr[5];				            // Declare array
+int sum = 0; 				// Variable to store sum
+cout << "Enter 5 numbers:\n";		// Prompt user
+for(int i = 0; i < 5; i++) {			// Loop for input
+cin >> arr[i];				// Store input in array
+}
+for(int i = 0; i < 5; i++) { 		// Loop for sum
+sum += arr[i];				// Add each element to sum
+}
+cout << "Sum = " << sum; 		// Display result
+return 0;					// End program
+}
+
+
